88f861df6b0f56bee16178eeb5459bc89116508e
Steven Spriggs <steven.spriggs@gmail.com>
2016-04-26 23:20:22 -0400

    fix rendering haml in layouts

--- lib/jekyll-haml/ext/convertible.rb
+++ lib/jekyll-haml/ext/convertible.rb
@@ -4,9 +4,28 @@
 
 module Jekyll
   class << Layout
-    alias old_initialize initialize
-    def initialize(*args)
-      old_initialize(*args)
+    # alias old_initialize initialize
+    # def initialize(*args)
+    #   old_initialize(*args)
+    #   self.content = self.transform
+    # end
+    def initialize(site, base, name)
+      @site = site
+      @base = base
+      @name = name
+
+      if site.theme && site.theme.layouts_path.eql?(base)
+        @base_dir = site.theme.root
+        @path = site.in_theme_dir(base, name)
+      else
+        @base_dir = site.source
+        @path = site.in_source_dir(base, name)
+      end
+
+      self.data = {}
+
+      self.process(name)
+      self.read_yaml(base, name)
       self.content = self.transform
     end
   end
